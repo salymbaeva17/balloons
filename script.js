b52d13b3a7ffe20b0743530ac141c1bb059be265
Aitolkun Salymbaeva <salymbaeva17@gmail.com>
2021-08-05 10:48:13 +0600

    initial

---
+++ script.js
@@ -0,0 +1,22 @@
+// const circleBtn = document.querySelector(".circle")
+// const colorBtn = document.querySelector(".color")
+// const borderRadius = document.querySelector(".border__radius")
+// const closeBtn = document.querySelector(".close")
+// const element = document.querySelector(".element")
+//
+//
+// circleBtn.addEventListener("click", () => {
+//     element.classList.toggle("do__circle")
+// })
+//
+// colorBtn.addEventListener("click", () => {
+//     element.classList.toggle("do__red")
+// })
+//
+// borderRadius.addEventListener("click", () => {
+//     element.classList.toggle("do__border-radius")
+// })
+//
+// closeBtn.addEventListener("click", () => {
+//     element.classList.toggle("do__invisible")
+// })
